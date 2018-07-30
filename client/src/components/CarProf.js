ffae1017704b0df94776166ebb3ba1d2b51e66b9
plaifang7 <plaifang7@gmail.com>
2018-07-30 12:49:25 -0400

    adding controller and routes for car show

--- client/src/components/CarProf.js
+++ client/src/components/CarProf.js
@@ -3,29 +3,39 @@ import axios from 'axios'
 
 class CarProf extends Component {
   state = {
-    cars: {},
-    carShows: []
+    car: {},
+    car_shows: []
 
   }
 
   async componentDidMount(){
-    let cars = []
-    cars = await this.showCar()
-    this.setState({cars})
+    let car = {}
+    let car_shows =[]
+
+    car = await this.showCar()
+    this.setState({
+      car, 
+      car_shows})
   }
 
   showCar = async () => {
     const carId = this.props.match.params.id
    const res = await axios.get(`/cars/${carId}`)
-   return res.data
+   return res.data 
+   
+  }
+  getCarShows = async (showId) => {
+    const carId = this.props.match.params.id
+    const res = await axios.get(``)
+
   }
 
   render() {
     return (
       <div>
         <div>
-       <h1>{this.state.cars.year} {this.state.cars.model}</h1>
-       <img src={this.state.cars.image} alt={this.state.cars.model} width ="400" />
+       <h1>{this.state.car.year} {this.state.car.model}</h1>
+       <img src={this.state.car.image} alt={this.state.car.model} width ="400" />
        </div>
        <div>
          Car Shows
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
 
  
-  resources :cars
+  resources :cars do
+    resources :car_shows
+  end
+
 end
